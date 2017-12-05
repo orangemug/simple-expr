ad523758b0e233ddd51ffd260f85539b7e7b60af
orangemug <info@orangemug.co.uk>
2017-12-05 18:03:21 +0000

    Rebuild.

--- docs/app.js
+++ docs/app.js
@@ -1,5 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-
+/**
+ * This parser was built from <https://github.com/thejameskyle/the-super-tiny-compiler>
+ */
 
 function tokenizer(input) {
   let current = 0;
@@ -33,25 +35,18 @@ function tokenizer(input) {
       continue;
     }
 
-    // Whitespace is insignificant
+    // Whitespace is ignored
+    // Note: whitespace in strings are handled separately in the string handler
     let WHITESPACE = /\s/;
     if (WHITESPACE.test(char)) {
       current++;
       continue;
     }
 
-		// TODO: Check commas here...
-
     let NUMBERS = /[-+.0-9]/;
     if (NUMBERS.test(char)) {
-
-      // We're going to create a `value` string that we are going to push
-      // characters to.
       let value = '';
 
-      // Then we're going to loop through each character in the sequence until
-      // we encounter a character that is not a number, pushing each character
-      // that is a number to our `value` and incrementing `current` as we go.
       while (NUMBERS.test(char)) {
         value += char;
         char = input[++current];
@@ -61,56 +56,22 @@ function tokenizer(input) {
         throw "Invalid number '"+value+"'"
       }
 
-      // After that we push our `number` token to the `tokens` array.
       tokens.push({ type: 'number', value });
-
-      // And we continue on.
       continue;
     }
 
-    // // Comment
-		// if (char === '/' && input[current+1] == "*") {
-    //   // Keep a `value` variable for building up our string token.
-    //   let value = '';
-
-    //   // We'll skip the opening double quote in our token.
-    //   char = input[current+=2];
-
-    //   // Then we'll iterate through each character until we reach another
-    //   // double quote.
-    //   while (!(char == "*" && input[current+1] == "/")) {
-    //     value += char;
-    //     char = input[++current];
-    //   }
-
-    //   // Skip the closing double quote.
-    //   char = input[current+=2];
-
-    //   // And add our `string` token to the `tokens` array.
-    //   tokens.push({ type: 'comment', value });
-
-    //   continue;
-		// }
-
-    // Feature reference
+    // Variable
 		if (char === '&') {
-      // Keep a `value` variable for building up our string token.
       let value = '';
 
-      // We'll skip the opening double quote in our token.
+      // Skip the '&'
       char = input[++current];
 
-      // Then we'll iterate through each character until we reach another
-      // double quote.
       while (char.match(/[a-zA-Z0-9_]/)) {
         value += char;
         char = input[++current];
       }
 
-      // Skip the closing double quote.
-      char = input[++current];
-
-      // And add our `string` token to the `tokens` array.
       tokens.push({ type: 'var_ref', value });
 
       continue;
@@ -118,22 +79,17 @@ function tokenizer(input) {
 
     // Feature reference
 		if (char === '@') {
-      // Keep a `value` variable for building up our string token.
       let value = '';
 
-      // We'll skip the opening double quote in our token.
+      // Skip the '@'
       char = input[++current];
 
-      // Then we'll iterate through each character until we reach another
-      // double quote.
       while (char.match(/[a-zA-Z0-9_]/)) {
         value += char;
         char = input[++current];
       }
 
-      // And add our `string` token to the `tokens` array.
       tokens.push({ type: 'feature_ref', value });
-
       continue;
 		}
 
@@ -144,8 +100,7 @@ function tokenizer(input) {
       // We'll skip the opening double quote in our token.
       char = input[++current];
 
-      // Then we'll iterate through each character until we reach another
-      // double quote.
+      // Iterate through each character until we reach another double quote.
       var prev;
       while (prev === "\\" || char !== '"') {
         value += char;
@@ -156,10 +111,13 @@ function tokenizer(input) {
         }
       }
 
+      if(char !== "\"") {
+        throw "Missing closing quote"
+      }
+
       // Skip the closing double quote.
       char = input[++current];
 
-      // And add our `string` token to the `tokens` array.
       tokens.push({ type: 'string', value });
 
       continue;
@@ -182,9 +140,7 @@ function tokenizer(input) {
       continue;
 		}
 
-    // Finally if we have not matched a character by now, we're going to throw
-    // an error and completely exit.
-    throw new TypeError('I dont know what this character is: ' + char);
+    throw new TypeError('I don\'t know what this character is: ' + char);
   }
 
 	return tokens;
@@ -193,14 +149,11 @@ function tokenizer(input) {
 function parser(tokens, depth) {
   depth = depth || 0;
 
-  // Again we keep a `current` variable that we will use as a cursor.
+  // Cursor
   let current = 0;
 
-  // But this time we're going to use recursion instead of a `while` loop. So we
-  // define a `walk` function.
   function walk() {
 
-    // Inside the walk function we start by grabbing the `current` token.
     let token = tokens[current];
     let prevToken = tokens[current-1];
 
@@ -210,28 +163,22 @@ function parser(tokens, depth) {
       || prevToken.type === "paren"
     )
 
-
     if(token.type === "arg_sep") {
       token = tokens[++current];
     }
     else if(!prevIsSep) {
+      // Any argument **must** pre proceeded with a separtor
       throw "Expecting argument separator";
     }
 
-
-
-    // We're going to split each type of token off into a different code path,
-    // starting off with `number` tokens.
-    //
-    // We test to see if we have a `number` token.
     if (token.type === 'number') {
 
       // If we have one, we'll increment `current`.
       current++;
 
-      // And we'll return a new AST node called `NumberLiteral` and setting its
-      // value to the value of our token.
       var value = token.value;
+
+      // Work out the type
       if(token.value.match(/[.]/)) {
         value = parseFloat(value, 10);
       }
@@ -245,8 +192,6 @@ function parser(tokens, depth) {
       };
     }
 
-    // If we have a string we will do the same as number and create a
-    // `StringLiteral` node.
     if (token.type === 'string') {
       current++;
 
@@ -274,19 +219,11 @@ function parser(tokens, depth) {
       };
     }
 
-    // Next we're going to look for CallExpressions. We start this off when we
-    // encounter an open parenthesis.
-
     if(
       token.type === 'name'
     ) {
-      // We'll increment `current` to skip the parenthesis since we don't care
-      // about it in our AST.
       token = tokens[current++];
 
-      // We create a base node with the type `CallExpression`, and we're going
-      // to set the name as the current token's value since the next token after
-      // the open parenthesis is the name of the function.
       let node = {
         type: 'CallExpression',
         name: token.value,
@@ -297,10 +234,10 @@ function parser(tokens, depth) {
         tokens[current].type === 'paren' &&
         tokens[current].value !== '('
       ) {
-        throw "Arrrgh";
+        throw "Missing opening parenthesis";
       }
 
-      // We increment `current` *again* to skip the name token.
+      // ... and skip the opening parenthesis.
       token = tokens[++current];
 
       while (
@@ -317,24 +254,20 @@ function parser(tokens, depth) {
       }
 
 
-      if(!token || token.type !== "paren") {
+      // Check there are some closing parenthesis
+      if(!token || token.type !== "paren" || token.value !== ")") {
         throw "Missing paren"
       }
-
-      // Skip the closing parenthesis.
+      // ... and skip the closing parenthesis.
       current++;
 
-      // And return the node.
       return node;
     }
 
-    // Again, if we haven't recognized the token type by now we're going to
-    // throw an error.
+    // Token not recognized
     throw new TypeError(token.type);
   }
 
-  // Now, we're going to create our AST which will have a root which is a
-  // `Program` node.
   let ast = {
     type: 'Program',
     body: [],
@@ -348,9 +281,7 @@ function parser(tokens, depth) {
     throw "Only allowed one top level expression";
   }
 
-  // At the end of our parser we'll return the AST.
   return ast;
-
 }
 
 function transformer(nodes) {
@@ -415,7 +346,7 @@ function codeGenerator(nodes) {
       out = "&"+node.value;
     }
     else {
-      throw "Foobar"
+      throw "Invalid node "+JSON.stringify(node);
     }
 
     return out;
@@ -490,7 +421,8 @@ var EXAMPLES = [
     code: [
       "interpolate(",
       "  linear(), zoom()",
-      "  2, rgb(255, 0, 0),",
+      "  0, rgb(0, 0, 0),",
+      "  11, rgb(255, 0, 0),",
       "  16, rgb(0, 255, 0),",
       "  22, rgb(0, 0, 255)",
       ")"
